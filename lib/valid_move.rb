ee69a3aa70af8b3d49d0cfdca12fd7e182629044
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-08-24 18:10:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,10 +8,10 @@ def valid_move?(board, index)
        end
    end
 
-   def position_taken?(board, index)
-         if board[index] == "X" || board[index] ==  "O"
-          true
-         elsif board[index] == " " || board[index] == "" || board[index] == nil
-          false
-         end
-   end
+  # def position_taken?(board, index)
+  #       if board[index] == "X" || board[index] ==  "O"
+  #        true
+  #       elsif board[index] == " " || board[index] == "" || board[index] == nil
+  #        false
+  #       end
+  # end
