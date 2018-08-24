e4bbae7a8c3d87edcd499f92922a7853655775da
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-08-24 18:08:51 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,25 +1,29 @@
-def valid_move?(board, index)
-  if !position_taken? && board[index].between?(1, 9)
-    true
-  else position_taken?(board, index)
-    false
-  end
-end
-
-# def valid_move?(board, index)
-#       if index == nil || index < 0 || index > 8
-#        false
-#      elsif position_taken?(board, index)
-#         false
-#       else
-#         true
-#       end
-#   end
 #
-#   def position_taken?(board, index)
-#         if board[index] == "X" || board[index] ==  "O"
-#          true
-#         elsif board[index] == " " || board[index] == "" || board[index] == nil
-#          false
-#         end
-#   end
+
+def valid_move?(board,index)
+#  if board[index] == " " || board[index] == nil || board[index] == ""
+#    false
+#  else board[index] == "X" || board[index] == "O"
+#     true
+#  end
+#
+#end
+
+
+ def valid_move?(board, index)
+       if index == nil || index < 0 || index > 8
+        false
+      elsif position_taken?(board, index)
+         false
+       else
+         true
+       end
+   end
+
+   def position_taken?(board, index)
+         if board[index] == "X" || board[index] ==  "O"
+          true
+         elsif board[index] == " " || board[index] == "" || board[index] == nil
+          false
+         end
+   end
